b6e9798b0c15339ea594431b64e0f1fdeaecae73
Habib Akinwale <habibakinwale@Habibs-MacBook-Pro.local>
2020-04-03 08:54:27 +0100

    ft: module, controllers, and services (http, log)

--- script.js
+++ script.js
@@ -1,92 +1,34 @@
-// JavaScript Design Patterns common in AngularJS
+// (function(){
+//     const app =   angular.module('app', []);
 
-/**
- * 1. Functions as Abstractions
- * We use the function, as a basis for Abstraction
- * We can pass functions around like any other variable
- */
-
-// let work = function () {
-//     console.log('working hard!');
-// };
-
-// let doWork = function (f) {
-//     console.log('starting');
-
-//     f();
-
-//     console.log('end');
-// };
-
-// doWork(work);
-
-
-
-/**
- * 2. Functions to build modules
- * Functions are useful abstractions in JS
- * But sometimes we need more for example: An object
- * that house both method and data
- * 
- * Module in CS: 
- * Some collection of components with features that you can put together to perform some job
- */
-
-//  const createWorker = function() {
-
-//     let task1 = () => {
-//         console.log('task1')
-//     };
-
-//     let task2 = () => {
-//         console.log('task2')
-//     }
-
-//     // revealing module pattern
-//      return {
-//         job1: task1,
-//         job2: task2
-//      };
-
-//  };
-
-//  let worker = createWorker();
-//  worker.job1();
-//  worker.job2();
-
-
-/**
- * Functions to avoid global variables - IIFE
- * IIFE: Immediately Invoked Function Expression
- * 
- * Global variables are bad. They easily become source of bugs
- * JS is a dynamic language and its very easy to overrite an existing variable
- * 
- * Perform functions without introducing any variable to the global scope
- * 
- * Program is the only global variable below
- */
-
-// (function () {
-//     const createWorker = function () {
-
-//         let task1 = () => {
-//             console.log('task1')
+//     app.controller('MainController', ['$scope', '$http', '$log', function($scope, $http, $log) {
+//         github_users_url = 'https://api.github.com/users/robconery';
+//         const onUserComplete = (response) => {
+//             $scope.user = response.data;
 //         };
-
-//         let task2 = () => {
-//             console.log('task2')
+//         const onError = function(error) {
+//             $scope.error = 'Could not fetch the user';
 //         }
 
-//         // revealing module pattern
-//         return {
-//             job1: task1,
-//             job2: task2
-//         };
-
-//     };
-
-//     let worker = createWorker();
-//     worker.job1();
-//     worker.job2();
+//         $http.get(github_users_url)
+//             .then(onUserComplete, onError);
+//     }]);
 // }());
+
+// OR
+
+angular.module('app', [])
+    .controller('MainController', function($scope, $http, $log) {
+        github_users_url = 'https://ap.github.com/users/robconery';
+        const onUserComplete = (response) => {
+            $log.info(response);
+            $scope.user = response.data;
+        };
+        const onError = function(error) {
+            $log.error(error);
+            $scope.error = 'Could not fetch the user';
+        }
+
+        $http.get(github_users_url)
+            .then(onUserComplete, onError);
+    });
